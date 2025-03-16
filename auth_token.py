7539b9fbc072eee7aef3ac81b5a06d91e0d10011
diyasini13 <diyasini@google.com>
2025-03-16 15:44:09 +0530

    added auth_tokentry.py

--- auth_token.py
+++ auth_token.py
@@ -29,11 +29,8 @@ def authentication():
     # Get the token from the query parameter
     # token = st.query_params.get("token", [""])[0]
     # token = st.query_params.get("token", [""])[0] if st.query_params.get("token", [""]) else ""
-    token = st.query_params.get("token")
-    if token is None:
-        token = ""
-    else:
-        token = token[0]
+    token = st.query_params["token"][0]
+   
     print("Token from new: " , token)
     if not token:
         st.error("Unauthorized access. Token not provided.")
